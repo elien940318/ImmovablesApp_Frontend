9a09a72f97a3dd58b39b71bd23048e940f5adabb
tig04157 <tig04157@naver.com>
2020-09-23 01:05:21 +0900

    sellbuy

--- Components/bottom/Bidding/Write/DetailModal/SellBuyModal.js
+++ Components/bottom/Bidding/Write/DetailModal/SellBuyModal.js
@@ -17,34 +17,42 @@ class SellBuyModal extends Component {
 
   render() {
     return (
-        <TouchableWithoutFeedback onPress={this.props.toggle}>
-        <View style={{
-              flex: 1,
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              }}>
-          <TouchableWithoutFeedback>
-            <View style={{justifyContent:'center', alignItems:'center',width: 300, height: 150, borderWidth:1, borderColor:'#a7a7a7', borderRadius:5, backgroundColor:'#c0c0c0'}}>
-              <Text style={{color:'#004aff',margin:5}}>카테고리를 선택하시오.</Text>
-              <TouchableOpacity style={styles.button} onPress={
+      <TouchableWithoutFeedback onPress={this.props.toggle}>
+      <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            }}>
+        <TouchableWithoutFeedback>
+          <View style={{justifyContent:'center', alignItems:'center',width: 300, height: 150, borderWidth:1,borderRadius:10 ,borderColor:'#a7a7a7', backgroundColor:'white'}}>
+            <View style={{width:'100%',flexDirection:'row', justifyContent:'space-around'}}>
+              <Text style={{width:'33%'}}/>
+              <Text style={{color:'black',margin:5, fontWeight:'bold'}}>카테고리를 선택하시오.</Text>
+              <View style={{width:'33%', justifyContent:'flex-end', flexDirection:'row'}}>
+              <Icon style={{color:'#FF5C3B'}}name="ios-checkmark-circle"></Icon>
+              <Text style={{width:'20%'}}/>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={
                 this.props.categoryBuyChanger,
                 this.props.buyClicker
                 
               }> 
-              <Text style={{color:'#004aff'}}>방 구하기</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={
+            <Text style={{color:'#FF5C3B'}}>방 구하기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={
                 this.props.categorySellChanger,
                 this.props.sellClicker
               }> 
-                <Text style={{color:'#004aff'}}>방 내놓기</Text>
-              </TouchableOpacity>
-            </View>
-          </TouchableWithoutFeedback>
-        </View>
-      </TouchableWithoutFeedback>
-    );
+              <Text style={{color:'#FF5C3B'}}>방 내놓기</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+
   }
 }
 
