ba80f149adc6e34d2745e0ad2a6bfb29811509fa
Changgi Kang <elien940318@gmail.com>
2020-09-27 02:01:56 +0900

    tokenChecker.js

---
+++ Components/Login/tokenChecker.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer} from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack'
+import firebase from 'firebase'
+
+class tokenChecker extends Component 
+{
+    tokenCheck = () => {
+        var user = firebase.auth().currentUser;
+        var name, email, photoUrl, uid, emailVerified;
+        if (user) {
+          // User is signed in.
+          name = user.displayName;
+          email = user.email;
+          photoUrl = user.photoURL;
+          emailVerified = user.emailVerified;
+          uid = user.uid;
+        } else {
+          // No user is signed in.
+          this.props.navigation.navigate('LoginScreen');
+        }
+    }
+}
+
+
+export default tokenChecker;
