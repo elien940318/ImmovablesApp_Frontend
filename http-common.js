6dadbffec1b184be7fcbd331cf5ffc59d4e147fd
och5351 <dhcksgo5319@naver.com>
2020-09-26 17:48:36 +0900

    delete http-common.js

--- http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-/* build test */
-export default axios.create({
-  baseURL: "http://192.168.0.4:3000/api",
-  // baseURL: "http://54.180.109.57:3000/api",
-  headers: {    "Content-type": "application/json"
-  }
-});
