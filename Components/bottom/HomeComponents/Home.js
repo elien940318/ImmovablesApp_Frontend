e9302b7af559ebc5e2d176908e6ad63289f5674d
Changgi Kang <elien940318@gmail.com>
2020-09-27 02:40:33 +0900

    component

--- Components/bottom/HomeComponents/Home.js
+++ Components/bottom/HomeComponents/Home.js
@@ -6,10 +6,11 @@ import ColumnCardComponent  from './ColumnCardComponent';
 import Carousel2 from './Carousel2'
 import myData from '../../Util/test.json';
 import styles from '../../css/bottom/HomComponents/HomeCSS.js';
-import * as firebase from 'firebase';
+import tokenChecker from '../../Login/tokenChecker.js';
 //const SLIDER_WIDTH = Dimensions.get('window').width;
 //const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 //const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
+
 
 export default class home extends Component {
 
@@ -35,20 +36,7 @@ export default class home extends Component {
               feeds
             })
         });
-
-        var user = firebase.auth().currentUser;
-        var name, email, photoUrl, uid, emailVerified;
-        if (user) {
-          // User is signed in.
-          name = user.displayName;
-          email = user.email;
-          photoUrl = user.photoURL;
-          emailVerified = user.emailVerified;
-          uid = user.uid;
-        } else {
-          // No user is signed in.
-          this.props.navigation.navigate('Login')
-        }
+        //this.tokenChecker.bind(this);
     }
  
     fetchFeeds() {
