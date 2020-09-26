14e8627244cff262f99181061613a8e5d85eae61
och5351 <dhcksgo5319@naver.com>
2020-09-26 17:36:27 +0900

    temp

--- http-common.js
+++ http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 /* build test */
 export default axios.create({
-  baseURL: "http://54.180.109.57:3000/api",
+  baseURL: "http://192.168.0.4:3000/api",
+  // baseURL: "http://54.180.109.57:3000/api",
   headers: {    "Content-type": "application/json"
   }
 });
